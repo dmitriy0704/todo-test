42f2b9f03f8543592704163da8522166fc25e8bd
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2024-11-16 11:15:15 +0300

    ....

--- src/main/java/todo/entity/Todo.java
+++ src/main/java/todo/entity/Todo.java
@@ -1,44 +1,70 @@
 package todo.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.lang.NonNull;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
 public class Todo {
 
+
+    //@GeneratedValue(strategy = GenerationType.UUID)
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private String id;
 
     @Column(nullable = false)
     private String title;
 
+    @NotBlank
+    @Column(nullable = false)
     private String description;
+    private LocalDateTime created;
+    private LocalDateTime modified;
 
-    private Timestamp created;
-
-    private Timestamp modified;
-
+    @NotBlank
     @Column(nullable = false)
     private String priority;
 
+    @NotBlank
     @Column(nullable = false)
-
     private String status;
+
     private boolean completed;
 
+    @NotBlank
     @Column(nullable = false)
     private String author;
 
+    @NotBlank
     @Column(nullable = false)
     private String executor;
 
     public Todo() {
+        LocalDateTime now = LocalDateTime.now();
+        this.id = UUID.randomUUID().toString();
+        this.created = now;
+        this.modified = now;
+    }
 
+    public Todo(String title,
+                String description,
+                String priority,
+                String status,
+                String author,
+                String executor) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.status = status;
+        this.author = author;
+        this.executor = executor;
     }
 }
